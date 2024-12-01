27dee5350ac3ed9805a2a5e06d1a38483719fd7e
Omar <jamlee977@outlook.com>
2024-12-01 21:26:11 +0200

    ffs

--- app/messages/[username]/MessageChat.tsx
+++ app/messages/[username]/MessageChat.tsx
@@ -101,7 +101,7 @@ export function MessageChat({
                     placeholder="Start a message"
                     value={newMessage}
                     onChange={(e) => setNewMessage(e.target.value)}
-                    className="flex-1 px-4 py-2 rounded-full bg-gray-100 dark:bg-background focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="flex-1 px-4 py-2 rounded-full bg-gray-100 dark:text-white dark:bg-background focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
                 <button
                     onClick={handleSendMessage}
